631447c12804d68a21552b2198be22fd5263a41e
pitboon <pitboon@gmail.com>
2020-02-21 00:23:05 +0800

    test

---
+++ CoreAPI/Controllers/LoginController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using CoreAPI.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+        
+        private string GenerateJasonWebToken(UserModel user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub,user.UserName),
+                new Claim(JwtRegisteredClaimNames.Email,user.EmailAddress),
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.GivenName,"Testing")
+            };
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Issuer"],
+                claims,
+                expires: DateTime.Now.AddDays(10),
+                signingCredentials: credentials);
+
+            var encodetoken = new JwtSecurityTokenHandler().WriteToken(token);
+            return encodetoken;
+        }
+
+        private UserModel AuthenticateUser(UserModel login)
+        {
+            UserModel User = null;
+            if (login.UserName == "manager" && login.Password == "1234")
+            {
+                User = new UserModel
+                {
+                    UserName = "manager",
+                    EmailAddress = "abc@abc.com"
+                };
+            }
+            return User;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] UserModel data)
+        {
+            UserModel login = new UserModel();
+            login.UserName = data.UserName;
+            login.Password = data.Password;
+            IActionResult response = Unauthorized();
+
+            var user = AuthenticateUser(login);
+            if (user != null)
+            {
+                var tokenStr = GenerateJasonWebToken(user);
+                response = Ok(new { token = tokenStr });
+            }
+            return response;
+
+            //var identity = HttpContext.User.Identity as ClaimsIdentity;
+            //IList<Claim> claim = identity.Claims.ToList();
+            //string str = claim[0].Value + ", " + claim[1].Value + ", " + claim[2].Value + "," + claim[3].Value;
+            //return str;
+        }
+
+        [Authorize]
+        [HttpGet("GetValue")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { _config["Jwt:Key"], _config["Jwt:Issuer"], _config["ConnectionString:Default"] };
+        }
+    }
+}
---
+++ CoreAPI/Controllers/TestingController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestingController : ControllerBase
+    {
+        // GET: api/Testing
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Testing/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Testing
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+    }
+}
